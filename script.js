5fda844ed6231e3bc5502cc5f9faeed1c72335ce
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-04-04 10:08:52 -0300

    Adicionando a funcionalidade de favoritar um filme
    Favoritando um filme ao clicar no icone de coração, preenchendo o coração, encontrando o objeto filme apartir do título do filme no documento HTML, e salvando os filmes favoritados no local Storage como um array

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 import APIKey from './js/ApiKey.js'
 import { searchMovieFromMoviesAPI } from './js/searchMovie.js'
+let movies
+let favoriteButtons
+const favoritedMovies =
+  JSON.parse(localStorage.getItem('favoritedMovies')) || []
+
 const movieElementContainer = document.querySelector('.movies')
 const searchMovieElement = document.querySelector('[data-search-movie]')
 
@@ -7,9 +12,14 @@ async function getPopularMoviesFromMoviesAPI() {
   const urlAPI = `https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}`
   const requestResponse = await fetch(urlAPI)
   const JSONResponse = await requestResponse.json()
-  const movies = JSONResponse.results
+  movies = JSONResponse.results
   movies.forEach(movie => {
+    movie.isFavorite = false
     createMovieHTMLElement(movie)
+    favoriteButtons = movieElementContainer.querySelectorAll(
+      '[data-movie-favorite]'
+    )
+    addFavoriteButtonFunction(favoriteButtons)
   })
 }
 
@@ -18,7 +28,8 @@ export function createMovieHTMLElement({
   poster_path,
   vote_average,
   release_date,
-  overview
+  overview,
+  isFavorite
 }) {
   const image = `https://image.tmdb.org/t/p/original${poster_path}`
   const year = release_date.slice(0, 4)
@@ -30,14 +41,17 @@ export function createMovieHTMLElement({
     alt="Logo do filme ${title} (${year})"
   />
   <div class="movie__header">
-    <h3 class="movie__title">${title} (${year})</h3>
+    <h3 class="movie__title" data-movie-title>${title} (${year})</h3>
     <div class="movie-header__status">
       <span
         class="movie-header-status__state movie-header-status__state--rating"
         >${vote_average.toFixed(1)}</span
       >
       <button
-        class="movie-header-status__state movie-header-status__state--favorite"
+        data-movie-favorite
+        class="movie-header-status__state movie-header-status__state--unfavorite ${
+          isFavorite ? '.movie-header-status__state--favorite' : ''
+        }"
       >
         Favoritar
       </button>
@@ -47,6 +61,34 @@ export function createMovieHTMLElement({
     <p class="movie-synopsis__text">${overview}</p>
   </div>
 </div>`
+}
+
+function addFavoriteButtonFunction(buttons) {
+  buttons.forEach(button =>
+    button.addEventListener('click', function () {
+      this.classList.toggle('movie-header-status__state--favorite')
+
+      const movieHeader = this.parentNode.parentNode
+      console.log(movieHeader)
+      const movieTitle = movieHeader
+        .querySelector('[data-movie-title]')
+        .textContent.replace(/\s\(\d\d\d\d\)/g, '')
+      console.log(movieTitle)
+
+      const movieIndex = movies.findIndex(movie => movie.title === movieTitle)
+      const favoritedMovie = movies[movieIndex]
+      console.log(favoritedMovie)
+      if (!favoritedMovie.isFavorite) {
+        favoritedMovie.isFavorite = true
+        favoritedMovies.push(favoritedMovie)
+        localStorage.setItem('favoritedMovies', JSON.stringify(favoritedMovies))
+      } else {
+        favoritedMovie.isFavorite = false
+        favoritedMovies.splice(movieIndex, 1)
+        localStorage.setItem('favoritedMovies', JSON.stringify(favoritedMovies))
+      }
+    })
+  )
 }
 
 document.addEventListener('keydown', async function (e) {
